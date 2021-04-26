55a567cfa3509115409565ae66033221ebb5d55d
Ricardo Simões <69405731+Sumb-03@users.noreply.github.com>
2021-04-26 10:10:09 +0100

    Commit

--- CSharp/aula1/Program.cs
+++ CSharp/aula1/Program.cs
@@ -6,8 +6,25 @@ namespace aula1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             
+            // Informação sobre o nome do vendedor, salário e total de vendas
+            Console.WriteLine("Insira o seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Insira o seu salário fixo: ");
+            int salario = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o total que fez em vendas durante o mês: ");
+            int vendas = int.Parse(Console.ReadLine());
+
+            // Display da informação e calculo do salário final do mês
+            Console.WriteLine("");
+            double vendas_1 = vendas * 0.15;
+            double salario_final = vendas_1 + salario;
+            Console.WriteLine($"O nome do vendedor é: {nome}");
+            Console.WriteLine($"O salário fixo do vendedor é: {salario}");
+            Console.WriteLine($"O salário final do mês após as comissões é: {salario_final}");
+            Console.ReadKey();
+
+
         }
     }
 }
---
+++ CSharp/aula1/ativ 1 ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ativ_1_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
